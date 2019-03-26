9475fa876cec5a159d289c75e3ee79f65c352a98
vishwaspuri <vishwaspuri09@gmail.com>
2019-03-27 01:35:16 +0530

    Debugged login error

--- blog_clone/urls.py
+++ blog_clone/urls.py
@@ -23,6 +23,6 @@ from django.conf.urls import url
 urlpatterns = [
     url(r'^admin/$', admin.site.urls),
     url(r'^$', include('blog.urls')),
-    url(r'^accounts/login/$', views.LoginView, name='login'),
-    url(r'^accounts/logout/$', views.LogoutView, name='logout', kwargs={'next_page':'/'}),
+    url(r'^accounts/login/$', views.LoginView.as_view(), name='login'),
+    url(r'^accounts/logout/$', views.LogoutView.as_view(), name='logout', kwargs={'next_page':'/'}),
 ]
